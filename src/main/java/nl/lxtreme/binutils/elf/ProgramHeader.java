fcdb9240546e48bf51f7658641c9c5aaf2c99000
August Nagro <augustnagro@gmail.com>
2016-07-31 10:36:13 -0500

    modified for rtt2

--- src/main/java/nl/lxtreme/binutils/elf/ProgramHeader.java
+++ src/main/java/nl/lxtreme/binutils/elf/ProgramHeader.java
@@ -7,46 +7,58 @@
  */
 package nl.lxtreme.binutils.elf;
 
-import java.io.*;
-import java.nio.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Represents information about the various segments in an ELF object.
  */
 public class ProgramHeader {
-    public final SegmentType type;
-    public final long flags;
-    public final long offset;
-    public final long virtualAddress;
-    public final long physicalAddress;
-    public final long segmentFileSize;
-    public final long segmentMemorySize;
-    public final long segmentAlignment;
+	public final SegmentType type;
+	public final long flags;
+	public final long offset;
+	public final long virtualAddress;
+	public final long offsetOfVirtualAddress;
+	public final long physicalAddress;
+	public final long offsetOfPhysicalAddress;
+	public final long segmentFileSize;
+	public final long offsetOfSegmentFileSize;
+	public final long segmentMemorySize;
+	public final long offsetOfSegmentMemorySize;
+	public final long segmentAlignment;
 
-    public ProgramHeader(ElfClass elfClass, ByteBuffer buf) throws IOException {
-        switch (elfClass) {
-            case CLASS_32:
-                type = SegmentType.valueOf(buf.getInt() & 0xFFFFFFFF);
-                offset = buf.getInt() & 0xFFFFFFFFL;
-                virtualAddress = buf.getInt() & 0xFFFFFFFFL;
-                physicalAddress = buf.getInt() & 0xFFFFFFFFL;
-                segmentFileSize = buf.getInt() & 0xFFFFFFFFL;
-                segmentMemorySize = buf.getInt() & 0xFFFFFFFFL;
-                flags = buf.getInt() & 0xFFFFFFFFL;
-                segmentAlignment = buf.getInt() & 0xFFFFFFFFL;
-                break;
-            case CLASS_64:
-                type = SegmentType.valueOf(buf.getInt() & 0xFFFFFFFF);
-                flags = buf.getInt() & 0xFFFFFFFFL;
-                offset = buf.getLong();
-                virtualAddress = buf.getLong();
-                physicalAddress = buf.getLong();
-                segmentFileSize = buf.getLong();
-                segmentMemorySize = buf.getLong();
-                segmentAlignment = buf.getLong();
-                break;
-            default:
-                throw new IOException("Unhandled ELF-class!");
-        }
-    }
+	public ProgramHeader(ElfClass elfClass, ByteBuffer buf, long programHeaderOffset) throws IOException {
+		switch (elfClass) {
+		case CLASS_32:
+			type = SegmentType.valueOf(buf.getInt() & 0xFFFFFFFF);
+			offset = buf.getInt() & 0xFFFFFFFFL;
+			virtualAddress = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfVirtualAddress = programHeaderOffset + buf.position() - Integer.BYTES;
+			physicalAddress = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfPhysicalAddress = programHeaderOffset + buf.position() - Integer.BYTES;
+			segmentFileSize = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfSegmentFileSize = programHeaderOffset + buf.position() - Integer.BYTES;
+			segmentMemorySize = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfSegmentMemorySize = programHeaderOffset + buf.position() - Integer.BYTES;
+			flags = buf.getInt() & 0xFFFFFFFFL;
+			segmentAlignment = buf.getInt() & 0xFFFFFFFFL;
+			break;
+		case CLASS_64:
+			type = SegmentType.valueOf(buf.getInt() & 0xFFFFFFFF);
+			flags = buf.getInt() & 0xFFFFFFFFL;
+			offset = buf.getLong();
+			virtualAddress = buf.getLong();
+			offsetOfVirtualAddress = programHeaderOffset + buf.position() - Long.BYTES;
+			physicalAddress = buf.getLong();
+			offsetOfPhysicalAddress = programHeaderOffset + buf.position() - Long.BYTES;
+			segmentFileSize = buf.getLong();
+			offsetOfSegmentFileSize = programHeaderOffset + buf.position() - Long.BYTES;
+			segmentMemorySize = buf.getLong();
+			offsetOfSegmentMemorySize = programHeaderOffset + buf.position() - Long.BYTES;
+			segmentAlignment = buf.getLong();
+			break;
+		default:
+			throw new IOException("Unhandled ELF-class!");
+		}
+	}
 }
--- src/main/java/nl/lxtreme/binutils/elf/SectionHeader.java
+++ src/main/java/nl/lxtreme/binutils/elf/SectionHeader.java
@@ -7,72 +7,98 @@
  */
 package nl.lxtreme.binutils.elf;
 
-import java.io.*;
-import java.nio.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Represents information about the various sections in an ELF object.
  */
 public class SectionHeader {
-    private final int nameOffset;
-    private String name;
+	private int nameOffset;
+	private String name;
+	private long offsetShift = 0;
+	private long size;
 
-    public final SectionType type;
-    public final long flags;
-    public final long virtualAddress;
-    public final long fileOffset;
-    public final long size;
-    public final int link;
-    public final int info;
-    public final long sectionAlignment;
-    public final long entrySize;
+	public SectionType type;
+	public final long flags;
+	public final long virtualAddress;
+	public final long offsetOfVirtualAddress;
+	public final long fileOffset;
+	public final long offsetOfFileOffset;
+	public final long offsetOfSize;
+	public final int link;
+	public final int info;
+	public final long sectionAlignment;
+	public final long entrySize;
 
-    public SectionHeader(ElfClass elfClass, ByteBuffer buf) throws IOException {
-        nameOffset = buf.getInt();
-        type = SectionType.valueOf(buf.getInt());
+	public SectionHeader(ElfClass elfClass, ByteBuffer buf, long sectionHeaderOffset) throws IOException {
+		nameOffset = buf.getInt();
+		type = SectionType.valueOf(buf.getInt());
 
-        if (elfClass == ElfClass.CLASS_32) {
-            flags = buf.getInt() & 0xFFFFFFFFL;
-            virtualAddress = buf.getInt() & 0xFFFFFFFFL;
-            fileOffset = buf.getInt() & 0xFFFFFFFFL;
-            size = buf.getInt() & 0xFFFFFFFFL;
-        } else if (elfClass == ElfClass.CLASS_64) {
-            flags = buf.getLong();
-            virtualAddress = buf.getLong();
-            fileOffset = buf.getLong();
-            size = buf.getLong();
-        } else {
-            throw new IOException("Unhandled ELF-class!");
-        }
+		if (elfClass == ElfClass.CLASS_32) {
+			flags = buf.getInt() & 0xFFFFFFFFL;
+			virtualAddress = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfVirtualAddress = sectionHeaderOffset + buf.position() - Integer.BYTES;
+			fileOffset = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfFileOffset = sectionHeaderOffset + buf.position() - Integer.BYTES;
+			size = buf.getInt() & 0xFFFFFFFFL;
+			offsetOfSize = sectionHeaderOffset + buf.position() - Integer.BYTES;
+		} else if (elfClass == ElfClass.CLASS_64) {
+			flags = buf.getLong();
+			virtualAddress = buf.getLong();
+			offsetOfVirtualAddress = sectionHeaderOffset + buf.position() - Long.BYTES;
+			fileOffset = buf.getLong();
+			offsetOfFileOffset = sectionHeaderOffset + buf.position() - Long.BYTES;
+			size = buf.getLong();
+			offsetOfSize = sectionHeaderOffset + buf.position() - Long.BYTES;
+		} else {
+			throw new IOException("Unhandled ELF-class!");
+		}
 
-        link = buf.getInt();
-        info = buf.getInt();
+		link = buf.getInt();
+		info = buf.getInt();
 
-        if (elfClass == ElfClass.CLASS_32) {
-            sectionAlignment = buf.getInt() & 0xFFFFFFFFL;
-            entrySize = buf.getInt() & 0xFFFFFFFFL;
-        } else if (elfClass == ElfClass.CLASS_64) {
-            sectionAlignment = buf.getLong();
-            entrySize = buf.getLong();
-        } else {
-            throw new IOException("Unhandled ELF-class!");
-        }
-    }
+		if (elfClass == ElfClass.CLASS_32) {
+			sectionAlignment = buf.getInt() & 0xFFFFFFFFL;
+			entrySize = buf.getInt() & 0xFFFFFFFFL;
+		} else if (elfClass == ElfClass.CLASS_64) {
+			sectionAlignment = buf.getLong();
+			entrySize = buf.getLong();
+		} else {
+			throw new IOException("Unhandled ELF-class!");
+		}
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setSize(long size) {
+		this.size = size;
+	}
 
-    void setName(ByteBuffer buf) {
-        if (nameOffset > 0) {
-            byte[] array = buf.array();
+	public long getSize() {
+		return size;
+	}
 
-            int end = nameOffset;
-            while (end < array.length && array[end] != 0) {
-                end++;
-            }
+	public void setOffsetShift(long offsetShift) {
+		this.offsetShift = offsetShift;
+	}
 
-            name = new String(array, nameOffset, end - nameOffset);
-        }
-    }
+	public long getOffsetShift() {
+		return offsetShift;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	void setName(ByteBuffer buf) {
+		if (nameOffset > 0) {
+			byte[] array = buf.array();
+
+			int end = nameOffset;
+			while (end < array.length && array[end] != 0) {
+				end++;
+			}
+
+			name = new String(array, nameOffset, end - nameOffset);
+		}
+	}
 }
